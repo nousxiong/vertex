c688d1bdc9ce62836e8bc4a69ab2d4c648d5bfe3
熊小磊 <xiongxiaolei@lightboat.games>
2025-12-09 15:53:34 +0800

    VertexWebSocketClientTest 测试 Reactor concotMap

--- vertex-spring-boot/src/test/kotlin/io/vertex/autoconfigure/web/client/VertexWebSocketClientTest.kt
+++ vertex-spring-boot/src/test/kotlin/io/vertex/autoconfigure/web/client/VertexWebSocketClientTest.kt
@@ -4,11 +4,6 @@ package io.vertex.autoconfigure.web.client
 
 import io.vertex.autoconfigure.core.VertexAutoConfiguration
 import io.vertex.autoconfigure.web.server.VertexServerAutoConfiguration
-import io.vertx.core.Vertx
-import io.vertx.kotlin.coroutines.dispatcher
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.reactive.asFlow
-import kotlinx.coroutines.reactor.asFlux
 import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
@@ -21,6 +16,7 @@ import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping
 import org.springframework.web.reactive.socket.WebSocketHandler
 import org.springframework.web.reactive.socket.WebSocketMessage
 import org.springframework.web.reactive.socket.WebSocketSession
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toMono
 import java.net.URI
@@ -38,6 +34,7 @@ import kotlin.test.assertEquals
     value = [
         "vertex.http.client.enabled=true",
         "vertex.http.client.websocket.enabled=true",
+        "vertex.http.server.deployments.threadingModel=EVENT_LOOP",
     ],
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
@@ -57,32 +54,95 @@ class VertexWebSocketClientTest {
         fun handlerMapping(): HandlerMapping {
             val handlerMapping = SimpleUrlHandlerMapping(mapOf(ECHO_URL to WebSocketHandler {
                 echoHandler(it)
-            }))
-            handlerMapping.order = -1
+            }), -1)
             return handlerMapping
         }
 
         private fun echoHandler(session: WebSocketSession): Mono<Void> {
-            val messages = session.receive()
-                .filter { it.type == WebSocketMessage.Type.TEXT }
-                .asFlow()
-                .map {
-                    val payload = it.payloadAsText
-                    session.textMessage(payload)
+            logger.info("Server session: ${session.id}")
+            val upFlux = session.receive().filter { it.type == WebSocketMessage.Type.TEXT }
+            // 使用 concatMap 或 flatMap 来处理
+            val messages = upFlux.concatMap {
+                val payload = it.payloadAsText
+                logger.info("Server Received: $payload")
+                if (payload.isEmpty()) {
+                    // 上行空，则不下行
+                    return@concatMap Mono.empty()
                 }
-                .asFlux(Vertx.currentContext().dispatcher())
+
+                when {
+                    payload.endsWith("x2") -> {
+                        val downMsg = payload.removeSuffix("x2")
+                        if (downMsg.isEmpty()) {
+                            // 如果仅有x2后缀，直接不下行
+                            Mono.empty()
+                        } else {
+                            // 返回2个echo
+                            Flux.just(
+                                session.textMessage(downMsg),
+                                session.textMessage(downMsg)
+                            )
+                        }
+                    }
+                    else -> {
+                        // 通用处理
+                        Flux.just(session.textMessage(payload))
+                    }
+                }
+            }
+//            val messages = upFlux
+//                .asFlow()
+//                .map {
+//                    val payload = it.payloadAsText
+//                    logger.info("Server Received: $payload")
+//                    session.textMessage(payload)
+//                }
+//                .asFlux(Vertx.currentContext().dispatcher())
             return session.send(messages)
         }
     }
 
     @Test
-    fun `should echo hello`() {
+    fun `should echo two hello and bye`() {
         webSocketClient.execute(URI("ws://localhost:$port$ECHO_URL")) { session ->
-            session.send(session.textMessage("hello").toMono())
-                .then(session.receive().next().map { it.payloadAsText })
+            val downFlux = session.receive()
+            session.send(session.textMessage("hellox2").toMono())
+                .then(downFlux.next().map { it.payloadAsText })
                 .doOnNext {
                     assertEquals("hello", it)
-                    logger.info(it)
+                    logger.info("Client Received echo: $it")
+                }
+                .then(downFlux.next().map { it.payloadAsText })
+                .doOnNext {
+                    assertEquals("hello", it)
+                    logger.info("Client Received echo: $it")
+                }
+                .then(session.send(session.textMessage("x2").toMono()))
+                .then(session.send(session.textMessage("bye").toMono()))
+                .then(downFlux.next().map { it.payloadAsText })
+                .doOnNext {
+                    assertEquals("bye", it)
+                    logger.info("Client Received echo: $it")
+                }
+                .then(session.close())
+        }.block(Duration.ofMillis(5000))
+    }
+
+    @Test
+    fun `should echo hello and bye`() {
+        webSocketClient.execute(URI("ws://localhost:$port$ECHO_URL")) { session ->
+            val downFlux = session.receive()
+            session.send(session.textMessage("hello").toMono())
+                .then(downFlux.next().map { it.payloadAsText })
+                .doOnNext {
+                    assertEquals("hello", it)
+                    logger.info("Client Received echo: $it")
+                }
+                .then(session.send(session.textMessage("bye").toMono()))
+                .then(downFlux.next().map { it.payloadAsText })
+                .doOnNext {
+                    assertEquals("bye", it)
+                    logger.info("Client Received echo: $it")
                 }
                 .then(session.close())
         }.block(Duration.ofMillis(5000))
